7bf83f3fa36f31fa5ed9b5640409d635ff01e7e1
k1ngjet3r <jeter.lin1208@gmail.com>
2021-03-23 06:50:09 +0800

    Update img_search.py

--- img_search.py
+++ img_search.py
@@ -18,19 +18,24 @@ def image_search(target_img, pattern, precision=0.8):
     # preprocess image
     target = cv2.imread(target_img, 0)
     template = cv2.imread(pattern, 0)
+    
     if target_img is None:
         raise FileNotFoundError('Image name {} cannot be found'.format(target_img))
     if template is None:
         raise FileNotFoundError('Image name {} cannot be found'.format(template))
 
     height, width = template.shape
+
     x_offset = width/2
     y_offset = height/2
 
     result = cv2.matchTemplate(target, template, cv2.TM_CCOEFF_NORMED)
+
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    
     if max_val < precision:
         return [-1, -1]
+    
     x, y = (max_loc[0]+x_offset, max_loc[1]+y_offset)
 
     return x, y
